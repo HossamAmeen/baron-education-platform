28045ebb3f287591bc3aa81cc32af1bc745bc44d
Hossam <hossam.mohamed@shelter-technology.com>
2025-02-26 23:53:17 +0200

    fix url issue

--- configuration/urls.py
+++ configuration/urls.py
@@ -1,12 +1,14 @@
 from rest_framework.routers import DefaultRouter
-
+from django.urls import path
 from configuration.api import (ConfigurationRetrieveView, ReviewViewSet,
                                SliderViewSet)
 
 router = DefaultRouter()
 router.register(r'sliders', SliderViewSet, basename="sliders")
 router.register(r'reviews', ReviewViewSet, basename="reviews")
-router.register(r'configuration',
-                ConfigurationRetrieveView.as_view(), basename='configuration')
+
 
 urlpatterns = router.urls
+urlpatterns = +[
+    path('configuration/', ConfigurationRetrieveView.as_view(), name='configuration-detail'),
+]
