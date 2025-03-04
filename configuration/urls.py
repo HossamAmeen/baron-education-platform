38c7f234cee00e8a4aac4418c5da7d1d765f53a1
Hossam <hossam.mohamed@shelter-technology.com>
2025-03-04 23:42:10 +0200

    fix contact us import'

--- configuration/urls.py
+++ configuration/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from configuration.api import (ConfigurationRetrieveView, ReviewViewSet,
+from configuration.api import (ConfigurationRetrieveView, ReviewViewSet, ContactUsViewSet,
                                SliderViewSet)
 
 router = DefaultRouter()
