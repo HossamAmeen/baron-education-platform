342f78028308af012c024444ebf542adff260c60
Hossam <hossam.mohamed@shelter-technology.com>
2025-04-12 17:59:22 +0200

    add static url

--- config/urls.py
+++ config/urls.py
@@ -34,4 +34,4 @@ urlpatterns = [
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(
         url_name='schema'), name='swagger-ui')
 
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
