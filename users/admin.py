29c2008436719749eb3483ba7772ce8905fbb713
Hossam <hossam.mohamed@shelter-technology.com>
2025-04-21 01:33:10 +0200

    make password not required

--- users/admin.py
+++ users/admin.py
@@ -5,7 +5,7 @@ from django import forms
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'password-field'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'password-field'}), required=False)
 
     class Meta:
         model = User
@@ -13,7 +13,8 @@ class UserForm(forms.ModelForm):
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data['password'])
+        if self.cleaned_data['password']:
+            user.set_password(self.cleaned_data['password'])
         if commit:
             user.save()
         return user
