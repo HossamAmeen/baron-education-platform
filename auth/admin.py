e696e9d4690f999cce332207020a2fa1dbc05156
Hossam <hossam.mohamed@shelter-technology.com>
2025-04-20 23:06:48 +0200

    update student form

--- auth/admin.py
+++ auth/admin.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-from auth.models import PasswordReset
+# from django.contrib import admin
+# from auth.models import PasswordReset
 
-class PasswordResetAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'created')
-    search_fields = ('email',)
-    list_filter = ('created',)
+# class PasswordResetAdmin(admin.ModelAdmin):
+#     list_display = ('__str__', 'created')
+#     search_fields = ('email',)
+#     list_filter = ('created',)
 
-admin.site.register(PasswordReset, PasswordResetAdmin)
+# admin.site.register(PasswordReset, PasswordResetAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from users.models import User, Student
+from django import forms
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'password-field'}))
+
+    class Meta:
+        model = User
+        exclude = ('is_superuser', 'is_staff', 'role', 'last_login', 'groups', 'user_permissions')
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name', 'phone', 'parent_phone')
     search_fields = ('phone', 'parent_phone')
     list_filter = ('phone', 'parent_phone')
+    exclude = ('is_superuser', 'is_staff', 'role', 'last_login', 'groups', 'user_permissions')
+    form = UserForm
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name', 'phone', 'email', 'role')
