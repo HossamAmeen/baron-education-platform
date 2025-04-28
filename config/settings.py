9e68227241dc3994ac55150318803786ea024db5
Hossam <hossam.mohamed@shelter-technology.com>
2025-04-28 15:56:49 +0300

    add title and description in REST

--- config/settings.py
+++ config/settings.py
@@ -66,6 +66,8 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
+    "TITLE": "Baron API Documentation",
+    "DESCRIPTION": "This is the API documentation for Baron Learning platform.",
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
