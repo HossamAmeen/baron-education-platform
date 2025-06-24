d470a9cdff7a4bf73ced777bd7d563af0fe3f774
Hossam <hossam.mohamed@shelter-technology.com>
2025-06-24 11:15:56 +0300

    add studnet course

--- course/serializers.py
+++ course/serializers.py
@@ -101,7 +101,7 @@ class RetrieveCourseSerializer(serializers.ModelSerializer):
             "is_authenticated"
         ]
 
-    def get_is_authenticated():
+    def get_is_authenticated(self, obj):
         user = self.context["request"].user
         return True if user and user.is_authenticated else False
 
