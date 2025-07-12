5b65d53282512c95b10ee751fbce3be1f74fcc0c
Hossam <hossam.mohamed@shelter-technology.com>
2025-07-12 19:56:13 +0300

    update is student permission with role check

--- shared/permisions.py
+++ shared/permisions.py
@@ -3,4 +3,4 @@ from rest_framework.permissions import BasePermission
 
 class IsStudent(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_student
+        return request.user.role == "student"
